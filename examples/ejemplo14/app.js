bc36d527ffe0370ec55cb0361a95d8bf6ec17b5b
Leonardo <leomicheloni@hotmail.com>
2014-11-01 15:16:54 -0300

    nuevos ejemplos

---
+++ examples/ejemplo14/app.js
@@ -0,0 +1,24 @@
+var miapp = angular.module('miapp', []);
+
+miapp.controller('CountryCtrl', function ($scope) {
+	$scope.countries = [{
+			"name" : "China",
+			"population" : 1359821000
+		}, {
+			"name" : "India",
+			"population" : 1205625000
+		}, {
+			"name" : "United States of America",
+			"population" : 312247000
+		}, {
+			"name" : "Brazil",
+			"population" : 201032714
+		}, {
+			"name" : "Argentina",
+			"population" : 42192500
+		}, {
+			"name" : "Chile",
+			"population" : 17819054
+		}
+	];
+});
